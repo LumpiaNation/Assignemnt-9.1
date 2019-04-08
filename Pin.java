388e85aaae8c02d261fb5a20155be1fede143716
LumpiaNation <39897299+LumpiaNation@users.noreply.github.com>
2019-04-08 13:15:44 -0700

    making the pin

--- Pin.java
+++ Pin.java
@@ -1,1 +1,30 @@
 
+import java.util.Random;
+
+public class Pin
+{
+    private int[] ranNumbers = new int [10];
+    private int[] passEncrypt = new int [5];
+
+    public Pin(String passWord)
+    {
+
+    }
+}
+private void encryptPass(String passWord)
+{
+    String[] passSplit = passWord.split("");
+
+    for (int i = 0; i < passSplit.length; i++)
+    {
+        int indice = Integer.parseInt(passSplit[i]);
+        passEncrypt[i] = ranNumbers[indice];
+    }
+}
+
+private void newNumbers()
+{
+    Random random = new Random();
+
+    
+}
